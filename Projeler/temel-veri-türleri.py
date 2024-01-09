912a379e6892bef3bc34900a75fa21a96fabfee7
Adil Çebi <adilhamza.cebi@ogrenci.bartin.edu.tr>
2024-01-10 02:39:43 +0300

    Python-Proje

---
+++ Projeler/temel-veri-türleri.py
@@ -0,0 +1,62 @@
+#BASİT HESAP MAKİNESİ
+
+# sayi1 = int(input("Birinci Sayıyı Giriniz:"))
+# sayi2 = int(input("İkinci Sayıyı Giriniz:"))
+# sonuc = 0
+
+# print("1.Toplama","2.Çıkarma","3.Çarpma","4.Bölme")
+# islem = int(input("Yapılacak işlemi seçiniz:"))
+
+# if islem == 1:
+#     sonuc = sayi1 + sayi2
+#     print(sonuc)
+# elif islem == 2:
+#     sonuc = sayi1 - sayi2
+#     print(sonuc)
+# elif islem == 3:
+#     sonuc = sayi1 * sayi2
+#     print(sonuc)
+# elif islem == 4:
+#     if sayi1 and sayi2 != 0:
+#         sonuc = sayi1 / sayi2
+#         print(sonuc)
+#     else:
+#         print("0 ile bölme yapılamaz.")
+# else:
+#     print("Geçersiz bir değer girdiniz!")
+        
+#________________________________________________________________________________________________
+
+
+#BİR SAYI'NIN 2 YE BÖLÜMÜNDEN KALANI BULAN BİR PROGRAM
+
+# sayi = int(input("Bir Sayı Giriniz:"))
+# islem = sayi % 2
+# print("Kalan Sayı:", islem)
+
+#________________________________________________________________________________________________
+
+
+#Ondalık sayıları tam sayıya çeviren bir program.
+
+# sayi = float(input("Bir ondalık sayı giriniz: "))
+
+# print("1. Ondalık kısmı yuvarla")
+# print("2. Sadece tam sayıyı al")
+
+# islem = int(input("Yapılacak işlemi seçiniz: "))
+
+# if islem == 1:
+#     yuvarlama = round(sayi)
+#     print(f"Yuvarlanmış değer: {yuvarlama}")
+# elif islem == 2:
+#     tam_sayi = int(sayi)
+#     print(f"Sadece tam sayı: {tam_sayi}")
+# else:
+#     print("Geçersiz işlem seçimi!")
+
+
+
+
+
+    
