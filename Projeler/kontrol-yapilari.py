b04e745aaacd6b4d3b5b7af803ecf35792995210
Adil Çebi <adilhamza.cebi@ogrenci.bartin.edu.tr>
2024-01-12 01:45:24 +0300

    Update kontrol-yapilari.py

--- Projeler/kontrol-yapilari.py
+++ Projeler/kontrol-yapilari.py
@@ -1,16 +1,16 @@
 #Kullanıcıdan bir karakter girişi alın. Eğer bu karakter bir harf ise, "Bu bir harftir." yazdırın; eğer sayı ise, "Bu bir sayıdır."; diğer durumlarda "Bu bir özel karakterdir." yazdırın.
 
-karakter = input("Bir karakter giriniz: ")
+# karakter = input("Bir karakter giriniz: ")
 
-if karakter.isalpha(): 
-    print("Bu bir harftir.")
-elif karakter.isdigit():  
-    print("Bu bir sayıdır.")
-else: 
-    print("Bu bir özel karakterdir.")
+# if karakter.isalpha(): 
+#     print("Bu bir harftir.")
+# elif karakter.isdigit():  
+#     print("Bu bir sayıdır.")
+# else: 
+#     print("Bu bir özel karakterdir.")
 
 
-
+#_________________________________________________________________________________________________________________
 
 
 
